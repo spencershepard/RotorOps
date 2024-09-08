37edc11c4337a2d3b7b5f4b3592e0c4740787bc2
spencershepard <spencershepard@hotmail.com>
2024-09-07 18:21:22 -0700

    prep for defense

--- scripts/RotorOps.lua
+++ scripts/RotorOps.lua
@@ -1137,7 +1137,7 @@ function RotorOps.assessUnitsInZone(var)
      local halt_convoy = false
      local convoy_status = "enroute"
 
-     if #attacking_ground_units > 0 and not RotorOps.defending and RotorOps.halt_convoy_without_airsupport then
+     if #attacking_ground_units > 0 and RotorOps.halt_convoy_without_airsupport then
       if RotorOps.convoy_suppressor then
         if RotorOps.convoy_suppressor:isExist() then
           halt_convoy = true
@@ -1146,7 +1146,7 @@ function RotorOps.assessUnitsInZone(var)
           RotorOps.convoy_suppressor = nil
         end
 
-      elseif not RotorOps.predAirSupportNearActive() then
+      elseif not RotorOps.predAirSupportNearActive() and not RotorOps.defending then
         convoy_status = "waiting_for_escort"
         halt_convoy = true
       end
